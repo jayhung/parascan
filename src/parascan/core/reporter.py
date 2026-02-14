7a47d503496039a5da9688bd60299aec29718275
Jay Hung <jayhung@users.noreply.github.com>
2026-02-13 18:33:23 -0800

    add shield icon

--- src/parascan/core/reporter.py
+++ src/parascan/core/reporter.py
@@ -280,7 +280,13 @@ async def generate_html_report(scan_id: int) -> str:
     </style>
 </head>
 <body>
-    <h1>parascan Report</h1>
+    <h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#4ade80" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display:inline-block;vertical-align:middle;margin-right:0.25rem">
+            <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/>
+            <path d="m9 12 2 2 4-4"/>
+        </svg>
+        parascan Report
+    </h1>
     <p class="meta">
         Scan #{scan.id} &mdash; {_escape(scan.target_url)}<br>
         {scan.started_at.strftime('%Y-%m-%d %H:%M UTC') if scan.started_at else ''} &mdash;
